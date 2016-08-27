368c45fdb74f465213768b9dc2e20614a60948e8
server <server@server.com>
2016-08-27 13:45:13 -0300

    a

---
+++ IntroJava/src/semana2/tarea1/Actividad1.java
@@ -0,0 +1,26 @@
+package semana2.tarea1;
+
+public class Actividad1 {
+
+	public static void main(String[] args) 
+	{
+		
+                      String nombre ="Maria";
+				      
+                      String apellido= "Morena";
+				      
+                      int a=380;
+				      int b=200;
+				      int suma = a + b;
+				      
+				       
+				      System.out.print ("Yo soy" + nombre +  apellido);
+				      System.out.print ("Suma de a+b:"+ suma);
+				      
+				      
+
+
+
+	}
+
+}
